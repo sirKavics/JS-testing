7336239abdd1f7f8636c75cbf6fa1da6ce4e8953
sirkavics <falf555@gmail.com>
2024-10-04 17:57:42 -0400

    continuing JS crash course

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 // DRY = Don't repeat yourself
 
-let count = 1
-while (count <= 100) {
-    console.log(count)
-    count = count + 1;
-}
+// let count = 1
+// while (count <= 100) {
+//     console.log(count)
+//     count = count + 1;
+// }
+
+ for (let i = 1; i <= 20; i++) {
+     if (i % 3 === 0 && i % 5 === 0) {
+         console.log(`${i} -> Frontend Simplified`)
+     }
+    else if (i % 3 === 0) {
+        console.log(`${i} -> Frontend`)
+    }
+    else if (i % 5 === 0) {
+        console.log(`${i} - Simplified`)
+    }
+    else {
+        console.log(`${i} -> ${i}`)
+    }
+ }
