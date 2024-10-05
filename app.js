063a8be483f49ee06531b14e683e60f74c43423b
sirkavics <falf555@gmail.com>
2024-10-05 09:21:38 -0400

    JS crash course continuation

--- app.js
+++ app.js
@@ -6,17 +6,26 @@
 //     count = count + 1;
 // }
 
- for (let i = 1; i <= 20; i++) {
-     if (i % 3 === 0 && i % 5 === 0) {
-         console.log(`${i} -> Frontend Simplified`)
-     }
+/**
+ 
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplified`)
+    }
     else if (i % 3 === 0) {
         console.log(`${i} -> Frontend`)
     }
     else if (i % 5 === 0) {
-        console.log(`${i} - Simplified`)
+        console.log(`${i} -> Simplified`)
     }
     else {
         console.log(`${i} -> ${i}`)
-    }
- }
+}
+
+*/
+
+const str = "Frontend Simplified"
+
+for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
+}
