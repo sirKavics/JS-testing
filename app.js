5ccf0758cabce1c1809d50cf55909fefc1f04ed5
sirkavics <falf555@gmail.com>
2024-10-07 02:59:14 -0400

    continuing js crash course

--- app.js
+++ app.js
@@ -6,8 +6,10 @@
 //     count = count + 1;
 // }
 
+
+
 /**
- 
+ *
 for (let i = 1; i <= 20; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log(`${i} -> Frontend Simplified`)
@@ -21,19 +23,46 @@ for (let i = 1; i <= 20; i++) {
     else {
         console.log(`${i} -> ${i}`)
 }
-
+*
 */
-/** 
+
+
+
+/**
+ *  
 const str = "Frontend Simplified"
 
 for (let i = 0; i < str.length; ++i) {
     console.log(str[i])
 }
+*
 */
 
+
+
+/** 
+ *  
 function convertCelsiusToFahrenheit(celsius) {
-    let fahrenheit = celsius * 1.8 + 32
-    return fahrenheit
+    return fahrenheit = celsius * 1.8 + 32
 }
 
+console.log(convertCelsiusToFahrenheit(0))
+console.log(convertCelsiusToFahrenheit(10))
 console.log(convertCelsiusToFahrenheit(30))
+*
+*/
+
+
+
+let arr = [20 ,30, 40, 50, 100]
+
+// First element of array:
+console.log(arr[0])
+
+//Last element of array:
+console.log(arr[arr.length - 1])
+
+//Add element onto end of array:
+arr.push(200)
+
+console.log(arr)
