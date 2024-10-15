0239b58ab49f98721327dcf4b02176f4b88415ab
sirkavics <falf555@gmail.com>
2024-10-15 17:14:32 -0400

    async/await promise

--- app.js
+++ app.js
@@ -215,17 +215,27 @@ function changeTitleToRed() {
 */
 
 // PROMISES //
-
 //fetch("https://jsonplaceholder.typicode.com/users/1")
 const emailRef = document.querySelector(".email");
+/**
 
 // 1. Then
 fetch("https://jsonplaceholder.typicode.com/users/1")
-    .then((response) => {
+.then((response) => {
     return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-    });
+})
+.then((data) => {
+    console.log(data);
+    emailRef.innerHTML = data.email
+});
 
-// 2. Async / Await
+*/
+// 2. Async /Await
+async function main() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML = data.email
+}
+
+main();
